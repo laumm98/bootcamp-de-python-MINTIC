bdfffe5435efb92655168ef4b673a2af99f97a62
laumm98 <ing.lauramontana@gmail.com>
2024-07-15 20:13:13 -0500

    se abordaron fundamentos de python

---
+++ fundamentos.py
@@ -0,0 +1,12 @@
+name = "Jheyson"
+print (type(name))
+print (name[0])
+print (name[1])
+print (name[2])
+last_name = "Galvis Valencia"
+print (name * 5)
+print (len(name))
+print(name.lower())
+print(name.upper())
+print(name.strip())
+print(name + " "+ last_name)
