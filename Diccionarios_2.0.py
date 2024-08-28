70e2882611350baac802373b76b7330b47119b09
laumm98 <ing.lauramontana@gmail.com>
2024-08-28 17:20:10 -0500

    se trabajo con comparaciones

--- Diccionarios_2.0.py
+++ Diccionarios_2.0.py
@@ -22,3 +22,29 @@ print(type(claves))
 values = information.values()
 print(values)
 
+
+
+#TALLER COLABORATIVO
+
+contacts = { "nombre": "Laura Montaña",
+                    "edad": 27,
+                    "telefono": 3157849494,
+                    "signo zodiacal": "LEO",
+                    "Pelicula fav": "no tengo",
+                    "cancion fav": "Be Yourself - Audioslave",
+                    "comida fav": "Pasta a la carbonara",
+                    "¿Cuál es tu lugar soñado para unas vacaciones?": "Bali",
+                    "¿Qué habilidad secreta te gustaría tener?": "Telequinesis",
+                    "¿Tienes algún talento oculto o pasatiempo inusual?": "Reposteria",
+                    "¿Quién es tu héroe o persona que admiras más y por qué?": "ninguno en especial",
+                    "¿Cuál es el libro que más te ha impactado y por qué?": "El TUNEL - Ernesto Sabato",
+                    "¿Si pudieras cenar con cualquier persona, viva o muerta, quién sería y qué le preguntarías?": "Cenaria con mmmm Elon Musk y le preguntaria cual es la clave del exito",
+                    "¿Qué logro personal te enorgullece más hasta ahora?": "mi titulo profesional"
+
+}
+
+resultado = contacts.values()
+print(resultado)
+
+
+
---
+++ bucles_y_control_de_iteraciones.py
@@ -0,0 +1,34 @@
+numbers =[1, 2, 3, 4, 5, 6]
+
+#for i in numbers: 
+ #   print(i)
+
+#for i in numbers: 
+ #   print("i es igual a:", i+1)
+
+#for i in range (10):
+ #   print(i)   
+
+#for i in range (3, 10):
+ #   print(i)   
+
+fruits = ["manzana", "banano", "naranja", "fresa", "tomate"]
+
+for fruta in fruits:
+
+    print(fruta)
+    if fruta == "naranja":
+        print("fruta encontrada")
+
+x=0 
+
+#while x<5: 
+#    print(x)
+#    x += 1
+
+#con el break se detiene la linea el codigo cuando la condicion se cumple
+while x<5: 
+    if x == 3: 
+        break
+    print(x)
+    x += 1
